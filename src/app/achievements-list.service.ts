f2aa040082d46d551a50d8741cb1cae061de59d5
Влад <grishin.vlad23@gmail.com>
2019-11-24 02:10:37 +0200

    some stuff

--- src/app/achievements-list.service.ts
+++ src/app/achievements-list.service.ts
@@ -14,4 +14,21 @@ export class AchievementsListService {
     getAchievementsList() {
         return this.data;
     }
+
+    getProgressColor(percentage) {
+        let color;
+        if (typeof percentage === 'string') {
+            percentage = percentage.match(/\d+/g).join('');
+        }
+        if (percentage < 30) {
+            color = '#ff1212';
+        } else if (percentage < 50) {
+            color = '#FFA81E';
+        } else if (percentage <= 100) {
+            color = '#118B4B';
+        } else {
+            color = '#726692';
+        }
+        return color;
+    }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -17,23 +17,6 @@ export class ListComponent implements OnInit {
     constructor(private content: AchievementsListService) {
     }
 
-    getProgressColor(percentage) {
-        let color;
-        if (typeof percentage === 'string') {
-            percentage = percentage.match(/\d+/g).join('');
-        }
-        if (percentage < 30) {
-            color = '#ff1212';
-        } else if (percentage < 50) {
-            color = '#FFA81E';
-        } else if (percentage <= 100) {
-            color = '#118B4B';
-        } else {
-            color = '#726692';
-        }
-        return color;
-    }
-
     setAllCounters(achievementsArray) {
         achievementsArray.forEach((item) => {
             if (item.unlocked) {
